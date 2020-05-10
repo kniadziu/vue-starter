63180c931eb7ef054bab3fc4bd1b3649c1069279
Grzegorz Koziel <kniadziu@gmail.com>
2020-05-10 16:47:39 +0200

    initial

---
+++ src/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <label>Zaloguj się e-mailem</label>
+        <input type="email" v-model="email">
+        <button @click="enter()">{{ buttonLabelToDisplay }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['buttonLabel'],
+        data() {
+            return {
+                email: ''
+            }
+        },
+        methods: {
+            enter() {
+                this.$emit('login', this.email);
+            }
+        },
+        computed: {
+            buttonLabelToDisplay() {
+                return this.buttonLabel || 'Zaloguj się';
+            }
+
+            //opis
+        }
+    }
+</script>
